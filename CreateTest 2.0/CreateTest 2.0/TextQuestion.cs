3875f60929788dd28a63d2b3046bf04c01fbccb7
LearningSystem <sokol19945@gmail.com>
2015-11-30 02:30:33 +0500

    CreateTest

---
+++ CreateTest 2.0/CreateTest 2.0/TextQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateTest_2._0
+{
+   public class TextQuestion
+    {
+        public int num;
+        public string type_question;
+        public string question;
+        public string answ1;
+        public string answ2;
+        public string answ3;
+        public string answ4;
+        public string[] rightans=new string[4];
+        public int price;
+        public string[] part = new string[10];
+    }
+}
