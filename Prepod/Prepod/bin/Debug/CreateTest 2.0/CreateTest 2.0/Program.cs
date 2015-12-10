d8e1bf29a393fe3fa02f3524d1393a322d11374f
LearningSystem <sokol19945@gmail.com>
2015-12-10 17:03:45 +0500

    Обновление

---
+++ Prepod/Prepod/bin/Debug/CreateTest 2.0/CreateTest 2.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateTest_2._0
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+           // int num = Int32.Parse(args[0].ToString());
+            //if (args[1] != null)
+            //{
+            //    string PathXml = args[1].ToString();
+            //    Application.Run(new InfoTest(num, PathXml));
+            //}
+            //else
+            //    Application.Run(new InfoTest(num, null));
+            Application.Run(new InfoTest(1));
+        }
+    }
+}
