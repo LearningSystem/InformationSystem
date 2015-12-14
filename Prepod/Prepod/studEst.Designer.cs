2d3fb0213be6e5e3bc8112e86fd4de5141cb5ead
LearningSystem <sokol19945@gmail.com>
2015-12-14 15:13:46 +0500

    Дополнение

---
+++ Prepod/Prepod/studEst.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Prepod
+{
+    partial class studEst
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.моиОценкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рейтингToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(680, 246);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.моиОценкиToolStripMenuItem,
+            this.рейтингToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(680, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // моиОценкиToolStripMenuItem
+            // 
+            this.моиОценкиToolStripMenuItem.Name = "моиОценкиToolStripMenuItem";
+            this.моиОценкиToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.моиОценкиToolStripMenuItem.Text = "Мои оценки";
+            this.моиОценкиToolStripMenuItem.Click += new System.EventHandler(this.моиОценкиToolStripMenuItem_Click);
+            // 
+            // рейтингToolStripMenuItem
+            // 
+            this.рейтингToolStripMenuItem.Name = "рейтингToolStripMenuItem";
+            this.рейтингToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.рейтингToolStripMenuItem.Text = "Рейтинг";
+            this.рейтингToolStripMenuItem.Click += new System.EventHandler(this.рейтингToolStripMenuItem_Click);
+            // 
+            // studEst
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 270);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "studEst";
+            this.Text = "studEst";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.studEst_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem моиОценкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рейтингToolStripMenuItem;
+    }
+}
---
+++ Prepod/Prepod/studEst.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Prepod
+{
+    public partial class studEst : Form
+    {
+        SqlConnection conn;
+        SqlCommand comm;
+
+        string connectionString
+            = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+        SqlDataAdapter adapter;
+        DataTable data = new DataTable();
+        BindingSource bs = new BindingSource();
+
+        string numStud;
+        public studEst(string _numStud)
+        {
+            InitializeComponent();
+            numStud = _numStud;
+
+            conn = new SqlConnection(connectionString);
+            comm = new SqlCommand();
+            comm.Connection = conn;
+        }
+
+        private void loadInf()
+        {
+            conn.Open();            
+            comm.CommandText = "Select * from [Успеваемость по задачам]";
+
+            adapter = new SqlDataAdapter(comm);
+            adapter.Fill(data);
+
+            dataGridView1.DataSource = data;
+            dataGridView1.Columns[12].Visible = false;
+            dataGridView1.Columns[11].Visible = false;
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.Columns[3].Visible = false;
+            dataGridView1.Columns[0].Visible = false;
+
+            bs.DataSource = data;
+            bs.Filter = "[№ студента] = '" + numStud + "'";
+
+            conn.Close();
+        }
+
+        private void моиОценкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loadInf();
+        }
+
+        private void рейтингToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void studEst_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            studentWork sw = new studentWork(numStud);
+            sw.Show();
+            this.Hide();
+        }
+
+    }
+}
